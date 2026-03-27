4d655bc95d96db28ed746e32f2bc9108e40ac314
Mario <20222177@aloe.ulima.edu.pe>
2026-03-27 09:34:41 -0500

    updating models and services

--- schemas/Entities.py
+++ schemas/Entities.py
@@ -6,18 +6,19 @@ class Actividad(BaseModel):
     nombre: str
     es_fija: bool
     duracion_minutos: int
-    # Solo para actividades fijas
     inicio_minutos: Optional[int] = None 
     fin_minutos: Optional[int] = None
     tiempo_traslado_minutos: int = 0
+    dias_permitidos: List[int] 
 
 class HorarioRequest(BaseModel):
     actividades: List[Actividad]
-    hora_inicio_dia: int = 480 # 8:00 AM en minutos
-    hora_fin_dia: int = 1200   # 8:00 PM en minutos
+    hora_inicio_dia: int = 480
+    hora_fin_dia: int = 1200
 
 class ActividadProgramada(BaseModel):
     id_actividad: str
     nombre: str
+    dia: int
     inicio: int
-    fin: int                                                                                                                
+    fin: int
